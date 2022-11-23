71c1709d5208b7b567c8f62c3b0ab1a2f69f1e48
Xhavit <xhavit.osaj@outlook.fr>
2022-11-23 18:47:14 +0100

    edited client shop interface and move it to a package

---
+++ ClientRMI/src/fr/uge/eiffelbikestore/shop/IShop.java
@@ -0,0 +1,16 @@
+package fr.uge.eiffelbikestore.shop;
+
+import fr.uge.eiffelbikestore.bike.Bike;
+import fr.uge.eiffelbikestore.bike.IBike;
+import fr.uge.eiffelbikestore.person.PersonUGE;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface IShop extends Remote {
+    void addBike(long id, PersonUGE personUGE) throws RemoteException;
+    void deleteBike(long id) throws RemoteException;
+    List<IBike> lookBike() throws RemoteException;
+    Bike searchBike(long id) throws RemoteException;
+}
